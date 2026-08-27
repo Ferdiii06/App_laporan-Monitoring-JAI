dc33a73d723d97eb12317deeb3035c38330097a1
Ferdiii06 <fery883099@gmail.com>
2026-08-27 14:06:15 +0700

    test server unzen

--- config/reverb.php
+++ config/reverb.php
@@ -29,7 +29,7 @@ return [
     'servers' => [
 
         'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '10.62.231.23.'),
+            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
             'port' => env('REVERB_SERVER_PORT', 9001),
             'path' => env('REVERB_SERVER_PATH', ''),
             'hostname' => env('REVERB_HOST'),
